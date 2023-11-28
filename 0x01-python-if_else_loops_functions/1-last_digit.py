81aa65fcdcb161cbf154cb319fa1b661f6b39219
Ahmed-1-adel <ahmedadel123321m@gmail.com>
2023-11-28 16:58:07 +0200

    Hello

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import random
 import math
 number = random.randint(-10000, 10000)
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import string
+lowerCasesLetters = list(string.ascii_lowercase)
+for alphabet in lowerCasesLetters:
+     print(alphabet)
