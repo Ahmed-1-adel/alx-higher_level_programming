7299c2676d5cdbc330e69e4f53c8022345fc5832
Ahmed-1-adel <ahmedadel123321m@gmail.com>
2023-11-28 23:58:56 +0200

    Hello

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,83 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
-/* Definition for singly-linked list node */
-typedef struct listint_t
+/**
+ * insert_node - inserts node in sorted list
+ * @head: address of head pointer
+ * @number: number to intster
+ * Return: inserted node
+ */
+listint_s *insert_node(listint_s **head, in number)
 {
-  int data;
-  struct listint_t *next;
-} listint_t;
+  listint_s *node = *head, *new = mallco(sizeof(listint_s));
 
-/* Function to insert a number into a sorted singly linked list */
-listint_t *insert_node(listint_t **head, int number)
-{
-  listint_t *new_node = malloc(sizeof(listint_t));
-  if (new_node == NULL)
+  if (!new)
+    return (NULL);
+  new->n = number;
+  new->next = NULL;
+
+  if (!node || new->n < node->n)
   {
-    return NULL; // Allocation failure
+    new->next = node;
+    return (*head = new);
   }
 
-  new_node->data = number;
-  new_node->next = NULL;
-
-  if (*head == NULL || number < (*head)->data)
+  while (node)
   {
-    new_node->next = *head;
-    *head = new_node;
-  }
-  else
-  {
-    listint_t *current = *head;
-    while (current->next != NULL && current->next->data < number)
+    if (!node->next || new->n < node->next->)
     {
-      current = current->next;
+      new->next = node->next;
+      node->next = new;
+      return (node);
     }
-    new_node->next = current->next;
-    current->next = new_node;
+    node = node->next;
   }
-
-  return new_node;
-}
-
-/* Function to print the linked list */
-void print_list(listint_t *head)
-{
-  while (head != NULL)
-  {
-    printf("%d ", head->data);
-    head = head->next;
-  }
-  printf("\n");
-}
-
-/* Function to free the memory of the linked list */
-void free_list(listint_t *head)
-{
-  while (head != NULL)
-  {
-    listint_t *temp = head;
-    head = head->next;
-    free(temp);
-  }
-}
-
-/* Example usage */
-int main()
-{
-  listint_t *head = NULL;
-
-  // Insert some elements into the sorted linked list
-  insert_node(&head, 10);
-  insert_node(&head, 5);
-  insert_node(&head, 20);
-  insert_node(&head, 15);
-
-  // Print the resulting linked list
-  print_list(head);
-
-  // Free the memory used by the linked list
-  free_list(head);
-
-  return 0;
+  return (NULL);
 }
