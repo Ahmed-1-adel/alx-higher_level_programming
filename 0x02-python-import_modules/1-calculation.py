9e08627ad37a5c0c5f9085bebc80a699c8f562a6
Ahmed-1-adel <ahmedadel123321m@gmail.com>
2023-11-30 19:16:15 +0200

    Hello

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+def calc(a ,b):
+    if __name__ == "__main__"
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("Addition: {} + {} = {}".format(a, b, add(a, b)))
+    print("Subtraction{} - {} = {}".format(a, b, sub(a, b)))
+    print("Multiplication{} * {} = {}".format(a, b, mul(a, b)))
+    print("Division{} / {} = {}".format(a, b, div(a, b)))
