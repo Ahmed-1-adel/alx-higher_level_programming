50bb78d02eccc998514cf57542be7a71f03b3426
Ahmed-1-adel <ahmedadel123321m@gmail.com>
2023-12-03 19:35:02 +0200

    hello

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+def print_list_integer(my_list=[]):
+    if len(my_list) == 0:
+      print("Empty List")
+    else:
+      for num in my_list:
+          print("{}".format(num))
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+Solution 0x03-python-data_structures
