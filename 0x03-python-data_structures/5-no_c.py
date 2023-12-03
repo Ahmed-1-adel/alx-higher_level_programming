ec32887078bf04bb376d536bc2f66f9cf1db775a
Ahmed-1-adel <ahmedadel123321m@gmail.com>
2023-12-03 21:19:47 +0200

    Hello

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+def no_c(my_string):
+    listAllChars = list(my_string)
+    for char in listAllChars:
+        if char == "c" or char == "C":
+            listAllChars.remove(char)
+        else:
+            return("".join(listAllChars))
+    
