d35a5f9ca5026931fe2bd3074ea52ccb001e1ac1
Ahmed-1-adel <ahmedadel123321m@gmail.com>
2023-12-04 12:58:26 +0200

    Hello

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,5 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
       return (0, None)
     else:
-       return(len(sentence), sentence[0])
+       my_tuple = len(sentence), sentence[0]
+    return(my_tuple)
