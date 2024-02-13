c41175a7aead7516f576337c316be9363b5bce0f
Ahmed-1-adel <Ahmedadel123321m@gamil.com>
2024-02-13 21:13:21 +0200

    Hello

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+# Remove Database if not EXISTS
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+# List All Tables on A DATABASE
+-- execute: cat 3-list_tables.sql | mysql -hlocalhost -uroot -p mysql
+SHOW TABLE;
